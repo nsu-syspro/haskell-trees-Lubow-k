8bb54c1a491cfd27bad0ebe6aa4916e9b0446c91
Luba Kolbasova <luba1607@gmail.com>
2025-02-18 16:58:38 +0700

    2, 3 tasks done

--- src/Task2.hs
+++ src/Task2.hs
@@ -7,13 +7,13 @@ module Task2 where
 -- that are not supposed to be used in this assignment
 import Prelude hiding (compare, foldl, foldr, Ordering(..))
 
-import Task1 (Tree(..))
+import Task1 (Tree(..), torder, Order (InOrder))
 
 -- * Type definitions
 
 -- | Ordering enumeration
 data Ordering = LT | EQ | GT
-  deriving Show
+  deriving (Show, Eq)
 
 -- | Binary comparison function indicating whether first argument is less, equal or
 -- greater than the second one (returning 'LT', 'EQ' or 'GT' respectively)
@@ -33,7 +33,10 @@ type Cmp a = a -> a -> Ordering
 -- GT
 --
 compare :: Ord a => Cmp a
-compare = error "TODO: define compare"
+compare x y
+  | x == y = EQ
+  | x < y  = LT
+  | otherwise = GT
 
 -- | Conversion of list to binary search tree
 -- using given comparison function
@@ -46,7 +49,7 @@ compare = error "TODO: define compare"
 -- Leaf
 --
 listToBST :: Cmp a -> [a] -> Tree a
-listToBST = error "TODO: define listToBST"
+listToBST cmp = foldr (tinsert cmp) Leaf 
 
 -- | Conversion from binary search tree to list
 --
@@ -62,7 +65,7 @@ listToBST = error "TODO: define listToBST"
 -- []
 --
 bstToList :: Tree a -> [a]
-bstToList = error "TODO: define bstToList"
+bstToList = torder InOrder Nothing
 
 -- | Tests whether given tree is a valid binary search tree
 -- with respect to given comparison function
@@ -77,7 +80,14 @@ bstToList = error "TODO: define bstToList"
 -- False
 --
 isBST :: Cmp a -> Tree a -> Bool
-isBST = error "TODO: define isBST"
+isBST cmp tree = isSortedAscList cmp (bstToList tree) 
+
+isSortedAscList :: Cmp a -> [a] -> Bool 
+isSortedAscList cmp list = foldr (checkSorted cmp) True (zip list (tail list))
+
+checkSorted :: Cmp a -> (a, a) -> Bool -> Bool
+checkSorted cmp (current, next) acc = cmp current next == LT && acc
+
 
 -- | Searches given binary search tree for
 -- given value with respect to given comparison
@@ -95,7 +105,11 @@ isBST = error "TODO: define isBST"
 -- Just 2
 --
 tlookup :: Cmp a -> a -> Tree a -> Maybe a
-tlookup = error "TODO: define tlookup"
+tlookup _ _ Leaf   = Nothing
+tlookup cmp v (Branch x left right) 
+  | cmp v x == EQ  = Just x
+  | cmp v x == GT  = tlookup cmp v right
+  | otherwise      = tlookup cmp v left
 
 -- | Inserts given value into given binary search tree
 -- preserving its BST properties with respect to given comparison
@@ -113,7 +127,12 @@ tlookup = error "TODO: define tlookup"
 -- Branch 'a' Leaf Leaf
 --
 tinsert :: Cmp a -> a -> Tree a -> Tree a
-tinsert = error "TODO: define tinsert"
+tinsert _ v Leaf = Branch v Leaf Leaf
+tinsert cmp v (Branch x left right) 
+  | cmp v x == LT   = Branch x (tinsert cmp v left) right
+  | cmp v x == GT   = Branch x left (tinsert cmp v right)
+  | otherwise       = Branch v left right 
+
 
 -- | Deletes given value from given binary search tree
 -- preserving its BST properties with respect to given comparison
@@ -129,4 +148,28 @@ tinsert = error "TODO: define tinsert"
 -- Leaf
 --
 tdelete :: Cmp a -> a -> Tree a -> Tree a
-tdelete = error "TODO: define tdelete"
+tdelete _ _ Leaf = Leaf
+tdelete cmp v (Branch x left right) 
+  | cmp v x == LT   = Branch x (tdelete cmp v left) right
+  | cmp v x == GT   = Branch x left (tdelete cmp v right)
+  | otherwise       = ndelete cmp (Branch v left right)
+
+
+ndelete :: Cmp a -> Tree a -> Tree a
+ndelete _ Leaf                 =  error "deleting non-existent element"
+ndelete _ (Branch _ Leaf Leaf) =  Leaf         -- no children 
+ndelete _ (Branch _ Leaf rBranch) = rBranch    -- only right child
+ndelete _ (Branch _ lBranch Leaf) = lBranch    -- only left child
+ndelete cmp (Branch _ lBranch rBranch) = Branch minVal lBranch (tdelete cmp minVal rBranch)  -- two children (find min on the right)
+  where minVal = findMin rBranch
+
+
+findMin :: Tree a -> a
+findMin Leaf = error "deleting leaf"
+findMin (Branch _ (Branch x left right) _) = findMin (Branch x left right)
+findMin (Branch v Leaf _)                  = v
+
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ acc []   = acc           
+foldr f acc (x:xs) = f x (foldr f acc xs) 
